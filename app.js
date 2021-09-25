f3392c8079a52a6d967a2531ec642376444a6aa4
julybell <julybell16@gmail.com>
2021-09-25 15:52:20 +0300

    bad result

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ let roseToJack = '';
     });
 })( jQuery );
 
+
 let billy = (function(){
     let name = 'Billy';
     let girlfriendName = 'Rose';
@@ -26,13 +27,13 @@ let billy = (function(){
     return{
         sendMessage: function(){
             console.log('Billy sent a massage');
-           return $.publish(billyToRose);
+            $.publish(billyToRose);
+            return billyToRose;
         },
 
         subscribe: function(){
             
-            $.subscribe(roseToBill, function(e, results){
-                console.log('rose to bill: ', results);
+            $.subscribe(roseToBill, function(e){
                 console.log('AAAAAAAA RUN!!!');
             })
         
@@ -48,12 +49,13 @@ let jack = (function(){
 
     return{
         sendMessage: function(){
-            return $.publish(jackToRose);
+            $.publish(jackToRose);
+            return jackToRose;
         },
 
         subscribe: function(){
-            $.subscribe(roseToJack, function(e, results){
-                console.log('roseToJack', results);
+            $.subscribe(roseToJack, function(e){
+                console.log('roseToJack', roseToJack);
             })
         }
     }
@@ -67,23 +69,19 @@ let rose = (function(){
     return{
         subscribe: function(){
 
-            $.subscribe(jackToRose, function(e, results){
-                console.log(results);
-                return $.publish(roseToJack);
+            $.subscribe(jackToRose, function(e){
+                console.log(jackToRose);
+                $.publish(roseToJack);
             })
 
-            $.subscribe(billyToRose, function(e, results){
-                console.log(results);
-                return $.publish(roseToBill)
+            $.subscribe(billyToRose, function(e){
+                console.log(billyToRose);
+                $.publish(roseToBill)
             });
         }
     }
 })();
 
 console.log(billyToRose , jackToRose, roseToJack, roseToBill);
-billy.sendMessage();
-billy.subscribe();
-jack.sendMessage();
-jack.subscribe();
-rose.subscribe();
+
 
